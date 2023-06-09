2a29d573b86226241a82a065da40ef7bcfb1bba2
Maria Arkhipova <marias1403@yandex.ru>
2023-06-10 04:31:13 +0900

    routes added to ideal scenario

--- src/pages/ProductListPage/ProductListPage.jsx
+++ src/pages/ProductListPage/ProductListPage.jsx
@@ -6,12 +6,13 @@ import PrimaryButton from '../../components/PrimaryButton/PrimaryButton';
 import ProgressBar from '../../components/ProgressBar/ProgressBar';
 import ProductList from '../../components/ProductList/ProductList';
 import BarcodeMismatchPopup from '../../components/BarcodeMismatchPopup/BarcodeMismatchPopup';
+import { useNavigate } from 'react-router-dom';
 
 const ProductListPage = ({ products }) => {
+    const navigate = useNavigate();
     const [isBarcodeMismatchPopupOpen, setIsBarcodeMismatchPopupOpen] = useState(false);
     const [scannedItems, setScannedItems] = useState(0);
     const totalItems = products.reduce((total, product) => total + product.count, 0);
-   
 
     function closePopup() {
         setIsBarcodeMismatchPopupOpen(false);
@@ -19,6 +20,14 @@ const ProductListPage = ({ products }) => {
 
     const handleProductItemClick = () => {
         setScannedItems(scannedItems + 1);
+    };
+
+    const handleFinishPackingButtonClick = () => {
+        navigate('/finishsession');
+    };
+
+    const handleHasProblemsButtonClick = () => {
+        navigate('/hasproblems');
     };
 
     return (
@@ -32,6 +41,7 @@ const ProductListPage = ({ products }) => {
                 title='Есть проблема'
                 disabled={false}
                 left='24px'
+                onClick={handleHasProblemsButtonClick}
             />
             <div className={styles.listWrapper}>
                 <h1 className={styles.title}>Сканируйте товары B-09 и упаковку</h1>
@@ -43,6 +53,7 @@ const ProductListPage = ({ products }) => {
                 disabled={false}
                 variant='yellow'
                 right='24px'
+                onClick={handleFinishPackingButtonClick}
             />
             <Footer isErrorCase={false} isBackButton={true} isKeyboard={true}/>
         </div>
