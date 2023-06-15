b58fb9969b32f79e2d216ba69f6d15979e5349d6
Maria Arkhipova <marias1403@yandex.ru>
2023-06-15 16:58:44 +0900

    feat: ProblemProductList

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './ProductList.module.css';
 import ProductItem from '../ProductItem/ProductItem';
 import { useLocation } from 'react-router-dom';
